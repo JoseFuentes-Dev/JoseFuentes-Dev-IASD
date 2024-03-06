6af53d71f444d4b6ff8776cb048e01d280591ac4
JoseFuentes-Dev <jocfuentes94@gmail.com>
2024-03-06 11:58:54 -0800

    acerca de

--- src/script.js
+++ src/script.js
@@ -68,67 +68,78 @@ window.addEventListener("scroll",function(){
 
 
 //priueba
-function scrollTo(pos) {
-	$('html, body').stop().animate({scrollTop: pos}, 500);
-  }
-  
-  function activeClass(target, className) {
-	  target.addClass(className).siblings().removeClass(className);
-  }
-  
-  function resizeHeight() {
-	const _sectionHeight = $(window).height();
-	 $(".tab-content .sec").height(_sectionHeight);
-  }
-  
-  function getOffsets() {
-	const _offsets = [];
-	$(".tab-content .sec").each(function (i) {
-	  _offsets[i] = $(this).offset().top;
-	});
-	return _offsets;
-  }
-  
-  
-  $(function() {
-	const tabButtons = $(".tab-navigation li");
-	let offsets = [];
+function Tabs(options){
 	
-	$(window).load(function () {
-	  resizeHeight();
-	  offsets = getOffsets();
-	});
-	
-	$(window).resize(function(){
-	  resizeHeight();
-	  offsets = getOffsets();
-	})
-  
-	//click (tab-navgigation-button)
-	tabButtons.click(function (event) {
-	  event.preventDefault();
-  
-	  const index = $(this).index();
-	  const pos = index > 0 ? offsets[index] : 0;
-	  scrollTo(pos);
-	});
-	
-  $(window).scroll(function () {
-	const pos = $(this).scrollTop();
-	let targetClass;
-	let activeTarget;
-  
-	 for (let i = 0; i < offsets.length; i++) {
-		if (pos <= offsets[0]) {
-		  targetClass = ".tab01";
-		  break;
-		} else if (pos > offsets[i - 1] && pos <= offsets[i]) {
-		  targetClass = ".tab0" + (i+1);
-		} else if (pos >= offsets[offsets.length]) {
-		  targetClass = ".tab05";
+	var tabs = document.querySelector(options.el);
+	var initCalled = false;
+	var tabNavigation = tabs.querySelector(".c-tabs-nav");
+	var tabNavigationLinks = tabs.querySelectorAll(".c-tabs-nav__link");
+	var tabContentContainers = tabs.querySelectorAll(".c-tab");
+
+	var marker = options.marker ? createNavMarker() : false;
+
+	var activeIndex = 0;
+
+  function init(){
+		if (!initCalled){
+			initCalled = true;
+
+			for (var i = 0; i < tabNavigationLinks.length; i++){
+    			var link = tabNavigationLinks[i];
+    			clickHandlerSetup(link, i)
+    		}
+
+    		if (marker){
+    			setMarker(tabNavigationLinks[activeIndex]);
+    		}
 		}
-	  }
-	  activeTarget = $(targetClass);
-	  activeClass(activeTarget, "active");
-	});
-  });
+	}
+
+	function clickHandlerSetup(link, index){
+    	link.addEventListener("click", function(e){
+    		e.preventDefault();
+    		goToTab(index);
+    	})
+    }
+
+    function goToTab(index){
+    	if (index >= 0 && index != activeIndex && index <= tabNavigationLinks.length){
+    		tabNavigationLinks[activeIndex].classList.remove('is-active');
+    		tabNavigationLinks[index].classList.add('is-active');
+    		
+    		tabContentContainers[activeIndex].classList.remove('is-active');
+    		tabContentContainers[index].classList.add('is-active');
+
+    		if (marker){
+    			setMarker(tabNavigationLinks[index]);
+    		}
+
+    		activeIndex = index;
+    	}
+    }
+
+    function createNavMarker(){
+    	var marker = document.createElement("div");
+    	marker.classList.add("c-tab-nav-marker");
+    	tabNavigation.appendChild(marker);
+    	return marker;
+    }
+
+    function setMarker(element){
+        marker.style.left = element.offsetLeft +"px";
+        marker.style.width = element.offsetWidth + "px";
+    }
+
+    return {
+    	init: init,
+    	goToTab: goToTab
+    }
+}
+
+
+var m = new Tabs({
+	el: "#tabs",
+	marker: true
+});
+
+m.init();
