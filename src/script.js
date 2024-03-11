142cb8c11858b85bf92aa8fc1af6d2a63e7ad175
JoseFuentes-Dev <jocfuentes94@gmail.com>
2024-03-10 19:16:24 -0700

    agregando carrusell

--- src/script.js
+++ src/script.js
@@ -156,3 +156,30 @@ function autoPlay() {
 
 let timer = setInterval(autoPlay, 8000);
 
+
+//prueba
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<i class='fa fa-caret-left'></i>",
+    "<i class='fa fa-caret-right'></i>"
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    500:{
+      items: 2
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 4
+    }
+  }
+})
