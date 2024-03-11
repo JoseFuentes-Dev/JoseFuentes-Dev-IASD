486a27f81e85e83a6f9ffd94619ba37778f894f0
JoseFuentes-Dev <jocfuentes94@gmail.com>
2024-03-11 03:34:25 -0700

    agregando inicio js

---
+++ src/inicio.js
@@ -0,0 +1,187 @@
+const menu = document.querySelector(".menu");
+const menuMain = menu.querySelector(".menu-main");
+const goBack = menu.querySelector(".go-back");
+const menuTrigger = document.querySelector(".mobile-menu-trigger");
+const closeMenu = menu.querySelector(".mobile-menu-close");
+let subMenu;
+menuMain.addEventListener("click", (e) =>{
+	if(!menu.classList.contains("active")){
+		return;
+	}
+  if(e.target.closest(".menu-item-has-children")){
+	   const hasChildren = e.target.closest(".menu-item-has-children");
+	 showSubMenu(hasChildren);
+  }
+});
+goBack.addEventListener("click",() =>{
+	 hideSubMenu();
+})
+menuTrigger.addEventListener("click",() =>{
+	 toggleMenu();
+})
+closeMenu.addEventListener("click",() =>{
+	 toggleMenu();
+})
+document.querySelector(".menu-overlay").addEventListener("click",() =>{
+	toggleMenu();
+})
+function toggleMenu(){
+	menu.classList.toggle("active");
+	document.querySelector(".menu-overlay").classList.toggle("active");
+}
+function showSubMenu(hasChildren){
+   subMenu = hasChildren.querySelector(".sub-menu");
+   subMenu.classList.add("active");
+   subMenu.style.animation = "slideLeft 0.5s ease forwards";
+   const menuTitle = hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
+   menu.querySelector(".current-menu-title").innerHTML=menuTitle;
+   menu.querySelector(".mobile-menu-head").classList.add("active");
+}
+
+function  hideSubMenu(){  
+   subMenu.style.animation = "slideRight 0.5s ease forwards";
+   setTimeout(() =>{
+	  subMenu.classList.remove("active");	
+   },300); 
+   menu.querySelector(".current-menu-title").innerHTML="";
+   menu.querySelector(".mobile-menu-head").classList.remove("active");
+}
+
+window.onresize = function(){
+	if(this.innerWidth >991){
+		if(menu.classList.contains("active")){
+			toggleMenu();
+		}
+
+	}
+}
+//navbar down
+
+window.addEventListener("scroll",function(){
+	
+ let header =  document.querySelector("header");
+  header.classList.toggle("down",window.scrollY>0);
+});
+
+
+
+// home
+
+const slides = document.querySelector(".slider").children;
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const indicator = document.querySelector(".indicator");
+let index = 0;
+
+prev.addEventListener("click", function () {
+  prevSlide();
+  updateCircleIndicator();
+  resetTimer();
+});
+
+next.addEventListener("click", function () {
+  nextSlide();
+  updateCircleIndicator();
+  resetTimer();
+});
+
+// create circle indicators
+function circleIndicator() {
+  for (let i = 0; i < slides.length; i++) {
+    const div = document.createElement("div");
+    div.innerHTML = i + 1;
+    div.setAttribute("onclick", "indicateSlide(this)");
+    div.id = i;
+    if (i == 0) {
+      div.className = "active";
+    }
+    indicator.appendChild(div);
+  }
+}
+circleIndicator();
+
+function indicateSlide(element) {
+  index = element.id;
+  changeSlide();
+  updateCircleIndicator();
+  resetTimer();
+}
+
+function updateCircleIndicator() {
+  for (let i = 0; i < indicator.children.length; i++) {
+    indicator.children[i].classList.remove("active");
+  }
+  indicator.children[index].classList.add("active");
+}
+
+function prevSlide() {
+  if (index == 0) {
+    index = slides.length - 1;
+  } else {
+    index--;
+  }
+  changeSlide();
+}
+
+function nextSlide() {
+  if (index == slides.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  changeSlide();
+}
+
+function changeSlide() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+
+  slides[index].classList.add("active");
+}
+
+function resetTimer() {
+  // when click to indicator or controls button
+  // stop timer
+  clearInterval(timer);
+  // then started again timer
+  timer = setInterval(autoPlay, 8000);
+}
+
+function autoPlay() {
+  nextSlide();
+  updateCircleIndicator();
+}
+
+let timer = setInterval(autoPlay, 8000);
+
+
+//carrousel
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<i class='fa fa-caret-left'></i>",
+    "<i class='fa fa-caret-right'></i>"
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    500:{
+      items: 2
+    },
+    740: {
+      items: 3
+    },
+    1000: {
+      items: 4
+    },
+    1400: {
+      items: 5
+    }
+  }
+})
